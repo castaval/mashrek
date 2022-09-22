0860ab43f5c985f1c155d3cbc25634409f2c5acc
castaval <alexander.butusov.03@mail.ru>
2022-09-22 17:06:29 +0300

    feat: add wizards for text, audio and photo

--- src/hears.ts
+++ src/hears.ts
@@ -1,37 +1,18 @@
 import { Markup, Context } from "telegraf";
 
-const items = ["./assets/mashrek-images/1.jpg", "./assets/mashrek-images/2.jpg", "./assets/mashrek-images/3.jpg", "./assets/mashrek-images/4.jpg", "./assets/mashrek-images/5.jpg", "./assets/mashrek-images/6.jpg", "./assets/mashrek-images/7.jpg", "./assets/mashrek-images/8.jpg", "./assets/mashrek-images/9.jpg", "./assets/mashrek-images/10.jpg"]
-
 export const Start = (ctx: any) => {
     ctx.replyWithPhoto(
         {source: "./assets/images/hello.jpg"},
         {caption: "Привет\nЯ Mashrek!\nНажми на кнопочки!",
             ...Markup.keyboard([["Хочу", "Не хочу",]]).resize().oneTime()});
-    ctx.scene.enter("hello");
 };
 export const Help = (ctx : Context) => ctx.reply("'Хочу' - получить картинку");
 export const Want = (ctx: any) => {
-    const item = items[Math.floor(Math.random()*items.length)]
-    ctx.replyWithPhoto(
-        {source: item},
-        {...Markup.removeKeyboard()});
-    ctx.scene.enter("work");
-};
-export const DontWant = (ctx: Context) => ctx.reply("Ну ладно (:\nЕсли захочешь, нажми на кнопочку 'Хочу' !");
-export const Introduction = (ctx: any) => {
     ctx.reply("Выберите взаймодействие",
         Markup.inlineKeyboard([
             Markup.button.callback("Фото", "photo"),
-            Markup.button.callback("Музыка", "music"),
+            Markup.button.callback("Музыка", "audio"),
             Markup.button.callback("Текст", "text"),
         ]));
 };
-export const PhotoAction = (ctx: any) => {
-
-};
-export const MusicAction = (ctx: any) => {
-
-};
-export const TextAction = (ctx: any) => {
-
-};
+export const DontWant = (ctx: Context) => ctx.reply("Ну ладно (:\nЕсли захочешь, нажми на кнопочку 'Хочу' !");
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,22 @@
-import { Start, DontWant, Help, Want, Introduction, PhotoAction, MusicAction, TextAction } from "hears";
+import { Start, DontWant, Help, Want } from "hears";
 import { Telegraf, Scenes, session } from 'telegraf';
 import { botToken } from "token";
+import { audioWizard, photoWizard, textWizard} from "wizards";
 
-const bot = new Telegraf<Scenes.SceneContext>(botToken);
-const helloScene = new Scenes.BaseScene<Scenes.SceneContext>("hello");
-const workScene = new Scenes.BaseScene<Scenes.SceneContext>("work");
-const stage = new Scenes.Stage<Scenes.SceneContext>([helloScene, workScene]);
+const bot = new Telegraf<Scenes.WizardContext>(botToken);
+const stage = new Scenes.Stage<Scenes.WizardContext>([textWizard, photoWizard, audioWizard]);
 
 bot.use(session());
 bot.use(stage.middleware());
 bot.launch();
 
 bot.start(Start);
-helloScene.help(Help);
-helloScene.hears('Хочу', Want);
-helloScene.hears('Не хочу', DontWant);
-workScene.enter(Introduction);
-workScene.action("photo", PhotoAction);
-workScene.action("music", MusicAction);
-workScene.action("text", TextAction);
+bot.help(Help);
+bot.hears('Хочу', Want);
+bot.hears('Не хочу', DontWant);
+bot.action("photo", ctx => ctx.scene.enter('photo-wizard'));
+bot.action("audio", ctx => ctx.scene.enter('audio-wizard'));
+bot.action("text", ctx => ctx.scene.enter('text-wizard'));
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
